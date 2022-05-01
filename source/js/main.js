21a8681a06be082d05697f9082f5d080c92e853c
Holder19 <Manhunt19@inbox.ru>
2022-05-01 13:20:55 +0300

    Выполняет верстку футера и делает аккордеон

--- source/js/main.js
+++ source/js/main.js
@@ -1,23 +1,46 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
+const accordions = document.querySelectorAll('.accordion');
+const accordionsTitles = document.querySelectorAll('#accordion-title');
 
-// ---------------------------------
+const accordionToggle = () => {
+
+  accordions.forEach((accordion) => {
+
+    if (accordion.classList.contains('accordion--nojs')) {
+      accordion.classList.remove('accordion--nojs');
+      accordion.classList.add('accordion--close');
+    }
+  });
+
+  accordionsTitles.forEach((accordionTitle) => {
+
+    accordionTitle.addEventListener('click', () => {
+
+      let currentAccordionTitle = accordionTitle;
+      let accordionTitleId = currentAccordionTitle.getAttribute('data-accordion');
+      let currentAccordionContent = document.querySelector(accordionTitleId);
+
+      if (currentAccordionContent.classList.contains('accordion--close')) {
+        accordions.forEach((item) => {
+          item.classList.add('accordion--close');
+          item.classList.remove('accordion--open');
+        });
+
+        currentAccordionContent.classList.remove('accordion--close');
+        currentAccordionContent.classList.add('accordion--open');
+      } else {
+        currentAccordionContent.classList.remove('accordion--open');
+        currentAccordionContent.classList.add('accordion--close');
+      }
+    });
+  });
+
+};
 
 window.addEventListener('DOMContentLoaded', () => {
 
-  // Utils
-  // ---------------------------------
-
   iosVhFix();
-
-  // Modules
-  // ---------------------------------
-
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
-  window.addEventListener('load', () => {
-    initModals();
-  });
+  accordionToggle();
 });
 
 // ---------------------------------
